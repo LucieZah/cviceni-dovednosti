e8d035b0f22edea20b976f94cbda2a26c345090d
Lucie Zahalkova <lucie.zahalkova@protonmail.com>
2024-03-10 19:03:28 +0100

    Posunuti urovne

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('funguju!');
+const html = prompt("Zadejte svou uroven znalosti HTML na urovni 0-100", "10")
+const css = prompt("Zadejte svou uroven znalosti CSS na urovni 0-100", "70")
+const javaScript = prompt("Zadejte svou uroven znalosti JavaScriptu na urovni 0-100", "100")
+
+const updateSkill = (selector, value) => {
+    const skillProgress = document.querySelector(selector + " .skill__progress")
+    skillProgress.style.width = value + "%"
+    const skillValue = document.querySelector(selector + " .skill__value")
+    skillValue.innerHTML = value + "/100"
+}
+
+updateSkill("#skill1", html)
+updateSkill("#skill2", css)
+updateSkill("#skill3", javaScript)
